d0f65afe794929f9a85f575b7aa3e34298085e8d
Tim Hopgood <thopgood@student.42lisboa.com>
2024-08-14 13:10:24 +0100

    WNOHANG on wait

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: thopgood <thopgood@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 18:55:41 by thopgood          #+#    #+#             */
-/*   Updated: 2024/08/14 12:44:57 by thopgood         ###   ########.fr       */
+/*   Updated: 2024/08/14 12:53:54 by thopgood         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ void	initialise_pipex_struct(int ac, char **av, char **envp, t_pipex *pipex)
 // TODO check every failure for leaks or fd leaks
 // TODO check for open files with lsof
 // TODO appending NULL to the end of argv in execve
+// TODO check fd.log with student (./pipex infile "cat -b" "./script" outfile)
 int	main(int ac, char **av, char **envp)
 {
 	t_pipex	pipex;
